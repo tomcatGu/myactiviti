3de61b3d1fc7225d86d5ca0b048987e2848f09ca
tomcat.gu <tomcat.gu@gmail.com>
2014-03-28 17:10:12 +0800

    form controller

---
+++ src/main/java/org/crusoe/mvc/ajax/form/FormController.java
@@ -0,0 +1,65 @@
+package org.crusoe.mvc.ajax.form;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.activiti.engine.FormService;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/runtime/form")
+public class FormController {
+
+	@Autowired
+	private RepositoryService repositoryService;
+	@Autowired
+	private RuntimeService runtimeService;
+	@Autowired
+	private ProcessEngine processEngine;
+	@Autowired
+	private FormService formService;
+
+	@RequestMapping(value = "submitStartForm/{processDefinitionId}", method = RequestMethod.POST)
+	public ModelAndView submitForm(@PathVariable String processDefinitionId,
+			HttpServletRequest request, HttpServletResponse response) {
+		Map<String, String> map = null;
+		Map<String, String[]> map1 = request.getParameterMap();
+		Map returnMap = new HashMap();
+		Iterator entries = map1.entrySet().iterator();
+		Map.Entry entry;
+		String name = "";
+		String value = "";
+		while (entries.hasNext()) {
+			entry = (Map.Entry) entries.next();
+			name = (String) entry.getKey();
+			Object valueObj = entry.getValue();
+			if (null == valueObj) {
+				value = "";
+			} else if (valueObj instanceof String[]) {
+				String[] values = (String[]) valueObj;
+				for (int i = 0; i < values.length; i++) {
+					value = values[i] + ",";
+				}
+				value = value.substring(0, value.length() - 1);
+			} else {
+				value = valueObj.toString();
+			}
+			returnMap.put(name, value);
+		}
+
+		formService.submitStartFormData(processDefinitionId, map);
+		return new ModelAndView();
+	}
+}
