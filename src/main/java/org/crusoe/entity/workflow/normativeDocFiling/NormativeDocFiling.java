f2726657c87b20394bbcc9d837ca4c33317a3b3e
tomcat.gu <tomcat.gu@gmail.com>
2014-10-10 22:57:19 +0800

    add number-check to statisticalSheet.html

---
+++ src/main/java/org/crusoe/entity/workflow/normativeDocFiling/NormativeDocFiling.java
@@ -0,0 +1,17 @@
+package org.crusoe.entity.workflow.normativeDocFiling;
+
+import java.util.Date;
+
+public class NormativeDocFiling {
+	private Long id;
+	private String fileName;
+	private String departmentName;
+	private String messageNumber;
+	private String fileProperty;
+	private String contentClassification;
+	private Date releaseDate;
+	private Date implementationDate;
+	private String isOpen;
+	private String status;
+	private Date createOn;
+}
