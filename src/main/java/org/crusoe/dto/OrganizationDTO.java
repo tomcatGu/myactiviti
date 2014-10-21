855cc0bbb8493e35a91bdd5559ed4d1da329c5bd
tomcat.gu <tomcat.gu@gmail.com>
2014-10-21 17:23:41 +0800

    add OrganizationDTO class

---
+++ src/main/java/org/crusoe/dto/OrganizationDTO.java
@@ -0,0 +1,76 @@
+package org.crusoe.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.hibernate.validator.constraints.Email;
+
+import com.google.common.collect.Lists;
+
+
+public class OrganizationDTO implements Serializable{
+	private String name;
+	private Long id;
+	private OrganizationDTO parent;
+
+	private List<UserDTO> users = Lists.newArrayList(); // 有序的关联对象集合
+
+	// @NotBlank
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public List<UserDTO> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<UserDTO> users) {
+		this.users = users;
+	}
+
+
+	public Long getId() {
+		// TODO Auto-generated method stub
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		// TODO Auto-generated method stub
+		this.id = id;
+
+	}
+
+
+	public OrganizationDTO getParent() {
+		return parent;
+	}
+
+	public void setParent(OrganizationDTO parent) {
+		this.parent = parent;
+	}
+
+}
