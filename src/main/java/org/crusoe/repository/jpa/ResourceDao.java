9669e6fc95ef63d51ea74981e9bd3d00cf26ff3c
tomcat.gu <tomcat.gu@gmail.com>
2014-11-03 08:10:46 +0800

    custom user and group manage at activiti

--- src/main/java/org/crusoe/repository/jpa/ResourceDao.java
+++ src/main/java/org/crusoe/repository/jpa/ResourceDao.java
@@ -2,9 +2,12 @@ package org.crusoe.repository.jpa;
 
 import org.crusoe.entity.Resource;
 import org.crusoe.entity.Role;
+import org.crusoe.entity.User;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface ResourceDao extends PagingAndSortingRepository<Resource, Long>, JpaSpecificationExecutor<Resource>  {
+
+	
 
 }
--- src/main/java/org/crusoe/service/ActivitiUserManager.java
+++ src/main/java/org/crusoe/service/ActivitiUserManager.java
@@ -1,5 +1,6 @@
 package org.crusoe.service;
 
+import java.util.Iterator;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -9,21 +10,28 @@ import org.activiti.engine.identity.Group;
 import org.activiti.engine.identity.User;
 import org.activiti.engine.impl.Page;
 import org.activiti.engine.impl.UserQueryImpl;
+import org.activiti.engine.impl.persistence.entity.GroupEntity;
 import org.activiti.engine.impl.persistence.entity.UserEntity;
 import org.activiti.engine.impl.persistence.entity.UserEntityManager;
 
+import org.crusoe.entity.Role;
 import org.crusoe.repository.jpa.ResourceDao;
 import org.crusoe.repository.jpa.RoleDao;
+import org.crusoe.repository.jpa.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.google.common.collect.Lists;
 
 @Component
 public class ActivitiUserManager extends UserEntityManager {
 	@Autowired
-	private ResourceDao userDao;
+	private UserDao userDao;
 
 	@Autowired
 	private RoleDao roleDao;
+	@Autowired
+	private AccountService accountService;
 
 	@Override
 	public Boolean checkPassword(String userId, String password) {
@@ -43,14 +51,34 @@ public class ActivitiUserManager extends UserEntityManager {
 	@Override
 	public List<Group> findGroupsByUser(String userId) {
 		// TODO Auto-generated method stub
-		return super.findGroupsByUser(userId);
+
+		org.crusoe.entity.User user = userDao.findByLoginName(userId);
+		List<Group> groups = Lists.newArrayList();
+		List<Role> roles = user.getRoles();
+		Iterator iter = roles.iterator();
+
+		while (iter.hasNext()) {
+			Role role = (Role) iter.next();
+			GroupEntity group = new GroupEntity();
+			group.setId(role.getId().toString());
+			group.setName(role.getName());
+			// group.setType(role.)
+			groups.add(group);
+
+		}
+
+		return groups;
 	}
 
 	@Override
-	public UserEntity findUserById(String userId) {
+	public UserEntity findUserById(String loginName) {
 		// TODO Auto-generated method stub
-
-		return null;
+		UserEntity userEntity = new UserEntity();
+		org.crusoe.entity.User user = userDao.findByLoginName(loginName);
+		userEntity.setId(user.getLoginName());
+		userEntity.setEmail(user.getEmail());
+		userEntity.setFirstName(user.getName());
+		return userEntity;
 	}
 
 	@Override
